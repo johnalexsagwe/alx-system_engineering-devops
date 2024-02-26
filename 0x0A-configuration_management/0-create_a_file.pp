dce275fb1a83e49d14312bc9280b6730e271a729
johnalexsagwe <johnsagwe@gmail>
2024-02-25 22:44:45 -0800

    Create Puppet manifest for file resource, ensuring '/tmp/school' is a file with mode 0744, owned by www-data, and containing the content 'I love Puppet'.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# A file resource that creates a file at `/tmp`
+file {'/tmp/school':
+    ensure  => 'file',
+    mode    =>'0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet"'
+}
